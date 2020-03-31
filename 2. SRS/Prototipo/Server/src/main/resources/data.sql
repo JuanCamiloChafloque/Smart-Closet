6a151d36b08b253f4a05ca8e1eaec024f39036d3
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-03-31 17:33:51 -0500

    Commit Creación Prototipo

---
+++ 2. SRS/Prototipo/Server/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into company (id, name) values (-1, 'company1');
+insert into company (id, name) values (-2, 'company2');
+insert into company (id, name) values (-3, 'company3');
+insert into company (id, name) values (-4, 'company4');
+
+insert into employee (id, name, age, salary, employer_id) values (-1, 'employee1', 40, 1000, -1);
+insert into employee (id, name, age, salary, employer_id) values (-2, 'employee2', 20, 2000, -1);
+insert into employee (id, name, age, salary, employer_id) values (-3, 'employee3', 30, 1500, -1);
+insert into employee (id, name, age, salary, employer_id) values (-4, 'employee4', 40, 1200, -1);
+insert into employee (id, name, age, salary, employer_id) values (-5, 'employee6', 50, 1500, -2);
+insert into employee (id, name, age, salary, employer_id) values (-6, 'employee7', 30, 1800, -2);
+insert into employee (id, name, age, salary, employer_id) values (-7, 'employee8', 20, 3000, -2);
+insert into employee (id, name, age, salary, employer_id) values (-8, 'employee9', 20, 2500, -2);
