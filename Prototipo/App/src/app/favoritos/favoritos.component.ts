69f310c676448283749511117fc90a5e82ec6ff9
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-04-13 00:16:37 -0500

    Commit finalización componente Favoritos

--- Prototipo/App/src/app/favoritos/favoritos.component.ts
+++ Prototipo/App/src/app/favoritos/favoritos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../usuario/services/usuario';
+import { Prenda } from '../prenda/services/prenda';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UsuarioService } from '../usuario/services/usuario.service';
+import { PrendaService } from '../prenda/services/prenda.service';
 
 @Component({
   selector: 'app-favoritos',
@@ -7,9 +12,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritosComponent implements OnInit {
 
-  constructor() { }
+  user: Usuario = undefined;
+  llegoUsuario = false;
+  llegoPrenda = false;
+  favoritas: Prenda[];
+  seccionSelected = 'Filtrar';
+  filter: Prenda[];
+  message = '';
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UsuarioService,
+    private prendaService: PrendaService
+  ) { }
 
   ngOnInit(): void {
+
+    this.userService.findByNickname(localStorage.getItem('User')).subscribe(
+      results => {
+        this.user = results;
+        this.inicializar();
+      }
+    );
+
+    this.prendaService.getPrendasFavoritas(localStorage.getItem('User')).subscribe(
+      results => {
+        this.favoritas = results;
+        this.filter = results;
+        this.llegaronFavoritas();
+        console.log(this.favoritas);
+      }
+    );
+
+  }
+
+  llegaronFavoritas() {
+    this.llegoPrenda = true;
+    if (this.favoritas.length === 0) {
+      this.message = 'No tienes ninguna prenda favorita';
+    }
+  }
+
+  inicializar() {
+    this.llegoUsuario = true;
+  }
+
+  displayFavorites() {
+    this.filter = [];
+    console.log(this.seccionSelected);
+    if (this.seccionSelected !== 'Todas') {
+      for (const prenda of this.favoritas) {
+        if (prenda.seccion === this.seccionSelected) {
+          this.filter.push(prenda);
+        }
+      }
+    } else {
+      for (const prenda of this.favoritas) {
+        this.filter.push(prenda);
+      }
+    }
+
+    if (this.filter.length === 0) {
+      if (this.seccionSelected === 'Todas') {
+        this.message = 'No tienes ninguna prenda favorita';
+      } else {
+        this.message = 'No tienes ningún favorito en la sección ' + this.seccionSelected;
+      }
+    } else {
+      this.message = '';
+    }
   }
 
 }
